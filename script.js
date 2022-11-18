b88f220d7206707c664149d4054e50706d35f531
Kurtmj93 <kurtmj93@gmail.com>
2022-11-18 11:36:52 -0500

    css created, html updated

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-/* # Module 03 Mini-Project: Rock, Paper, Scissors
-
-In this activity, you will work with a group to build a game using only JavaScript.
-
-## Instructions
-
-The completed application should meet the following criteria:
-
-* As a user, I want to play Rock, Paper, Scissors against an automated opponent.
-
-* As a user, I can enter R, P, or S to signify my choice of rock, paper, or scissors.
-
-* As a user, I expect the computer to choose R, P, or S in return.
-
-* As a user, I want the option to play again whether I win or lose.
-
-* As a user, I want to see my total wins, ties, and losses after each round.
-
-### Specifications
-
-* Must use the `alert()`, `confirm()`, and `prompt()` methods to collect user input and display information to the user.
-
-* The computer's selection must be random to ensure a fair game.
-*/
-
 //stores game history
 
 var gameHistory = {
@@ -30,6 +5,8 @@ var gameHistory = {
     losses: 0,
     ties: 0
 }
+
+//three options
 
 var options = ['Rock', 'Paper', 'Scissors'];
 
